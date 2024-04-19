f9db20d8bd8a312c5078fd0b0ef2b0d15ba783c6
Billy Dentith <billydentith@hotmail.co.uk>
2024-04-19 11:41:20 +0100

    Adds POST /api/topics endpoint

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,7 +1,27 @@
-const { selectTopics } = require('../models/topics.models');
+const { selectTopics, insertTopic } = require('../models/topics.models');
 
 exports.getAllTopics = (req, res, next) => {
     selectTopics().then((topics) => {
         res.status(200).send({ topics });
     }).catch(next)
+}
+
+exports.postTopic = (req, res, next) => {
+    const newTopic = req.body;
+    const acceptedProperties = ['slug', 'description'];
+    let validTopic = true;
+
+    Object.keys(newTopic).forEach((key) => {
+        if (!acceptedProperties.includes(key)) {
+            validTopic = false;
+        }
+    })
+
+    if (validTopic) {
+        insertTopic(newTopic).then((topic) => {
+            res.status(201).send({ topic })
+        }).catch(next)
+    } else {
+        res.status(400).send({ message: 'Invalid Topic' })
+    }
 }
