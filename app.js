b6580d45c045da6aacd4f2b645c45098b2c2f77f
Billy Dentith <billydentith@hotmail.co.uk>
2024-05-03 15:53:21 +0100

    Allows cross origin resource sharing

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 const express = require('express');
 const apiRouter = require('./routes/api-router');
 const userRouter = require('./routes/users-router');
@@ -7,6 +8,7 @@ const articleRouter = require('./routes/articles-router');
 const { handleInvalidEndpoint, handleCustomErrors, handlePsqlErrors, handleServerErrors } = require('./errors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
